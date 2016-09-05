b062eef1f48a30bab0d9b18d9a84f972acd6f968
Nikolay.Alekseev <nikolas@rain-studio.com>
2016-09-05 14:50:25 +0300

    Доработал 5-ю домашку

--- dz5/views/userslist.php
+++ dz5/views/userslist.php
@@ -2,7 +2,7 @@
 	<tr><td>#</td><td>ID</td><td>Имя</td><td>Совершеннолетность</td></tr>
 	<?
 	foreach ($list AS $line) {
-		$agest = ($line['age']>18) ? 'Совершеннолетний':'Несовершенноледний';
+		$agest = ($line['age']>=18) ? 'Совершеннолетний':'Несовершенноледний';
 		echo '<tr><td>#</td><td>'.$line['user']['id'].'</td><td>'.$line['name'].'</td><td>'.$agest.'</td></tr>';
 	}
 	?>
